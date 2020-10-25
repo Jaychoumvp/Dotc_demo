12126345125114434dcdf7c32cccf54a6669c132
13060229869@qq.com <1627717042@qq.com>
2020-10-25 13:56:20 +0800

    关注用户列表

--- app/resources/article/following.py
+++ app/resources/article/following.py
@@ -12,7 +12,7 @@ from common.utils.decorators import login_required
 
 class FollowUserResource(Resource):
     """关注用户"""
-    method_decorators = {'post': [login_required]}
+    method_decorators = {'post': [login_required], 'get': [login_required]}
 
     def post(self):
         # 获取参数
@@ -43,6 +43,54 @@ class FollowUserResource(Resource):
 
         # 返回结果
         return {'target': author_id}
+
+    def get(self):
+        """获取关注列表"""
+        # 获取参数
+        user_id = g.user_id
+        parser = RequestParser()
+        parser.add_argument('page', default=1, location='args', type=int)
+        parser.add_argument('per_page', default=2, location='args', type=int)
+        args = parser.parse_args()
+        page = args.page
+        per_page = args.per_page
+
+        # 查询数据 当前用户的关注列表
+        pn = User.query.options(load_only(User.id, User.name, User.profile_photo, User.fans_count)). \
+            join(Relation, User.id == Relation.author_id). \
+            filter(Relation.user_id == user_id, Relation.relation == Relation.RELATION.FOLLOW). \
+            order_by(Relation.update_time). \
+            paginate(page, per_page)
+
+        # 查询当前用户的粉丝列表
+        fans_list = Relation.query.options(load_only(Relation.user_id)). \
+            filter(Relation.author_id == user_id, Relation.relation == Relation.RELATION.FOLLOW).all()
+
+        # 序列化
+        author_list = []
+        for item in pn.items:
+            author_dict = {
+                'id': item.id,
+                'name': item.name,
+                'photo': item.profile_photo,
+                'fans_count': item.fans_count,
+                'mutual_follow': False
+            }
+
+            # 如果该作者也关注了当前用户, 则为互相关注
+            for fans in fans_list:
+                if item.id == fans.user_id:
+                    author_dict['mutual_follow'] = True
+                    break
+
+            author_list.append(author_dict)
+
+        # 返回数据
+        return {'results': author_list, 'per_page': per_page, 'page': pn.page, 'total_count': pn.total}
+
+
+
+
 
 
 class UnFollowUserResource(Resource):
