0466786bbc71dbee5ce4990e7fe94cc3877ac682
13060229869@qq.com <1627717042@qq.com>
2020-10-25 14:12:42 +0800

    评论列表

--- app/resources/article/comment.py
+++ app/resources/article/comment.py
@@ -5,6 +5,7 @@ from flask_restful.reqparse import RequestParser
 
 from app import db
 from common.models.article import Comment, Article
+from common.models.user import User
 from common.utils.decorators import login_required
 
 
@@ -34,3 +35,49 @@ class CommentsResource(Resource):
 
         # 返回结果
         return {'com_id': comment.id, 'target': target}
+
+    def get(self):
+        """获取评论列表"""
+
+        # 获取参数
+        parser = RequestParser()
+        parser.add_argument('source', required=True, location='args', type=int)
+        parser.add_argument('offset', default=0, location='args', type=int)
+        parser.add_argument('limit', default=10, location='args', type=int)
+        args = parser.parse_args()
+        source = args.source
+        offset = args.offset
+        limit = args.limit
+
+        # 查询文章的评论列表  需求: 分页
+        comments = db.session.query(Comment.id, Comment.user_id, User.name, User.profile_photo, Comment.ctime,
+                                    Comment.content, Comment.reply_count, Comment.like_count). \
+            join(User, Comment.user_id == User.id). \
+            filter(Comment.article_id == source, Comment.id > offset). \
+            limit(limit).all()
+
+        # 序列化
+        comment_list = [{
+            "com_id": item.id,
+            'aut_id': item.user_id,
+            'aut_name': item.name,
+            'aut_photo': item.profile_photo,
+            'pubdate': item.ctime.isoformat(),
+            'content': item.content,
+            'reply_count': item.reply_count,
+            'like_count': item.like_count
+        } for item in comments]
+
+        # 查询评论总数
+        total_count = Comment.query.filter(Comment.article_id == source).count()
+
+        # 所有评论中最后一条评论的id
+        end_comment = db.session.query(Comment.id).filter(Comment.article_id == source).order_by(
+            Comment.id).first()
+        end_id = end_comment.id if end_comment else None
+
+        # 获取本次请求最后一条数据的id
+        last_id = comments[-1].id if comments else None
+
+        # 返回数据
+        return {'results': comment_list, 'total_count': total_count, 'last_id': last_id, 'end_id': end_id}
